4163f9b2a2e53448bb886e80b6362231781bc1a2
Julian Nicholls <juliannicholls29@gmail.com>
2018-08-15 16:01:24 +0100

    Prepare for deployment

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
+const port = process.env.PORT || 5000
+
 app.get('/', (req, res) => {
   res.send({root: 'root'});
 });
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+app.listen(port, () => {
+  console.log('Server running on port', port);
 });
