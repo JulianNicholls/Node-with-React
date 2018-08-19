c64ac2edf830eab598d8f9b9adc2f1c49a3545f5
Julian Nicholls <juliannicholls29@gmail.com>
2018-08-19 20:16:14 +0100

    Rdirect to dashboard on login

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ module.exports = app => {
   );
 
   app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
-    res.redirect('/');
+    res.redirect('/surveys');
   });
 
   app.get('/api/logout', (req, res) => {
