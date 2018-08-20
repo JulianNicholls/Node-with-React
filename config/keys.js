a878a55fcb2276e9230d9f82916286cf64b44a76
Julian Nicholls <juliannicholls29@gmail.com>
2018-08-20 11:22:13 +0100

    Add Stripe keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 // keys.js
 
-if (process.env.NODE_ENV === 'production') module.exports = require('./prod');
-else module.exports = require('./dev');
+module.exports =
+  process.env.NODE_ENV === 'production' ? require('./prod') : require('./dev');
--- config/prod.js
+++ config/prod.js
@@ -5,5 +5,8 @@ module.exports = {
 
   mongoURI: process.env.MONGO_URI,
 
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
