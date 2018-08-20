7ddd6968ff87d2a866248319806032940de546ef
Julian Nicholls <juliannicholls29@gmail.com>
2018-08-20 15:46:00 +0100

    Revert payments change

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -4,23 +4,19 @@ import StripeCheckout from 'react-stripe-checkout';
 
 import { addCredits } from '../actions/auth';
 
-const Payments = ({ addCredits }) => {
-  console.log(process.env);
-
-  return (
-    <StripeCheckout
-      name="Emaily Survey Creator 2018"
-      image="/images/emaily-logo-tr.png"
-      description="5 credits for sending out survey emails"
-      amount={500} // Pennies
-      currency="USD" // For now...
-      token={token => addCredits(token)}
-      stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}
-    >
-      <button className="waves-effect waves-light btn">Add Credits</button>
-    </StripeCheckout>
-  );
-};
+const Payments = ({ addCredits }) => (
+  <StripeCheckout
+    name="Emaily Survey Creator 2018"
+    image="/images/emaily-logo-tr.png"
+    description="5 credits for sending out survey emails"
+    amount={500} // Pennies
+    currency="USD" // For now...
+    token={token => addCredits(token)}
+    stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}
+  >
+    <button className="waves-effect waves-light btn">Add Credits</button>
+  </StripeCheckout>
+);
 
 export default connect(
   null,
