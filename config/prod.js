f9d115d69f14b280e89981c521f87fb4ef91c673
Julian Nicholls <juliannicholls29@gmail.com>
2018-08-22 11:33:10 +0100

    Add sendGridKey to config keys

--- config/prod.js
+++ config/prod.js
@@ -8,5 +8,7 @@ module.exports = {
   cookieKey: process.env.COOKIE_KEY,
 
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+
+  sendGridKey: process.env.SENDGRID_API_KEY
 };
