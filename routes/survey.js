0b1dec1350f87e67f4fcd0b7d22dcdd5b8cf0d27
Julian Nicholls <juliannicholls29@gmail.com>
2018-08-24 11:54:41 +0100

    Update webhooks route code

--- routes/survey.js
+++ routes/survey.js
@@ -1,3 +1,7 @@
+const Path = require('path-parser').default;
+const { URL } = require('url');
+const _ = require('lodash');
+
 const mongoose = require('mongoose');
 
 const Mailer = require('../services/Mailer');
@@ -11,8 +15,8 @@ const Survey = mongoose.model('surveys');
 module.exports = app => {
   app.post('/api/add_survey', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
-    
-    const survey = await new Survey({
+
+    const survey = new Survey({
       title,
       subject,
       body,
@@ -20,42 +24,59 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now()
     });
-    
-    // Send the email(s)
+
+    // Send the email(s), save the survey, remove a credit, and return the user
 
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    
+
     try {
       await mailer.send();
-      
+
       await survey.save();
-      
+
       --req.user.credits;
       const user = await req.user.save();
-      
+
       res.send(user);
     } catch (err) {
       res.status(422).send(err);
     }
   });
-  
-  // [ { ip: '31.53.89.199', 
+
+  // Webhook post data
+  // [ { ip: '31.53.89.199',
   //    sg_event_id: 'PVqzYv6NQOWyelVcSVsTdQ',
   //    sg_message_id: '0pg8JUwWTCCIAB2xaLdm7Q.filter0038p3las1-22314-5B7FB90E-12.0',
-  //    useragent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium / 68.0.3440.106 Chrome / 68.0.3440.106 Safari / 537.36',
+  //    useragent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36
+  //                (KHTML, like Gecko) Ubuntu Chromium / 68.0.3440.106
+  //                Chrome / 68.0.3440.106 Safari / 537.36',
   //    event: 'click',
-  //    url_offset: { index: 0, type: 'html' }, 
-  //    email: 'juliannicholls@gmail.com', 
-  //    timestamp: 1535097157, 
+  //    url_offset: { index: 0, type: 'html' },
+  //    email: 'juliannicholls@gmail.com',
+  //    timestamp: 1535097157,
   //    url: 'http://localhost:3000/api/surveys/thanks'
   // } ]
-  
+
   app.post('/api/surveys/webhooks', (req, res) => {
-    console.log(req.body);
-    
-    res.send({ ok: true });
+    console.log('Events:', req.body.length);
+
+    const p = new Path('/api/surveys/:surveyID/:choice');
+
+    const events = _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+
+        return match ? { ...match, email } : null;
+      })
+      .compact()
+      .uniqBy('email', 'surveyID')
+      .value();
+
+    console.log(events);
+
+    res.send({ ok: 'true' });
   });
-  
+
   app.get('/api/surveys/:surveyid/:choice', (req, res) => {
     res.send('Thanks for responding.');
   });
