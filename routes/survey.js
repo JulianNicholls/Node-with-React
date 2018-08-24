164016be74926d19f1abc890ee5e331eae2be524
Julian Nicholls <juliannicholls29@gmail.com>
2018-08-24 09:15:01 +0100

    Add basic webhook routes

--- routes/survey.js
+++ routes/survey.js
@@ -9,13 +9,9 @@ const requireCredits = require('../middleware/requireCredits');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
-    res.send('Thanks for responding.');
-  });
-
   app.post('/api/add_survey', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
-
+    
     const survey = await new Survey({
       title,
       subject,
@@ -24,31 +20,44 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now()
     });
-
+    
     // Send the email(s)
 
     const mailer = new Mailer(survey, surveyTemplate(survey));
-
+    
     try {
       await mailer.send();
-
+      
       await survey.save();
-
+      
       --req.user.credits;
       const user = await req.user.save();
-
+      
       res.send(user);
     } catch (err) {
       res.status(422).send(err);
     }
   });
-
-  app.post('/api/set_credits', requireLogin, async (req, res) => {
-    req.user.credits = Math.min(req.body.credits, req.user.credits);
-
-    await req.user.save();
-
+  
+  // [ { ip: '31.53.89.199', 
+  //    sg_event_id: 'PVqzYv6NQOWyelVcSVsTdQ',
+  //    sg_message_id: '0pg8JUwWTCCIAB2xaLdm7Q.filter0038p3las1-22314-5B7FB90E-12.0',
+  //    useragent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium / 68.0.3440.106 Chrome / 68.0.3440.106 Safari / 537.36',
+  //    event: 'click',
+  //    url_offset: { index: 0, type: 'html' }, 
+  //    email: 'juliannicholls@gmail.com', 
+  //    timestamp: 1535097157, 
+  //    url: 'http://localhost:3000/api/surveys/thanks'
+  // } ]
+  
+  app.post('/api/surveys/webhooks', (req, res) => {
+    console.log(req.body);
+    
     res.send({ ok: true });
+  });
+  
+  app.get('/api/surveys/:surveyid/:choice', (req, res) => {
+    res.send('Thanks for responding.');
   });
 };
 
--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -8,12 +8,12 @@ module.exports = ({ body, id }) => {
       <h2>Please help us by answering the following question</h2>
       <h4>${body}</h4>
       <div style="float: left; margin-left: 40%; background: #338437; padding: 15px 20px; font-size: 150%">
-        <a style="color: white; text-decoration: none" href="${redirectDomain}/api/surveys/thanks">
+        <a style="color: white; text-decoration: none" href="${redirectDomain}/api/surveys/${id}/yes">
           Yes
         </a>
       </div>
       <div style="float: left; margin-left: 20px; background: #D32F2F; padding: 15px 20px; font-size: 150%">
-        <a style="color: white; text-decoration: none" href="${redirectDomain}/api/surveys/thanks">
+        <a style="color: white; text-decoration: none" href="${redirectDomain}/api/surveys/${id}/no">
           No
         </a>
       </div>
